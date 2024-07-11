a8310d527ad0d622913ca3f6104e54e3719dacc0
kuangyuwu <kuangyuwu95@gmail.com>
2024-07-10 20:03:07 -0500

    Chapter 1

--- main.go
+++ main.go
@@ -6,14 +6,26 @@ import (
 )
 
 func main() {
-	mux := http.NewServeMux()
+
+	const filepathRoot string = "."
 	const port string = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
